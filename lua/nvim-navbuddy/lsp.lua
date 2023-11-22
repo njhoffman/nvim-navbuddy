216f0c8923718b23c89ef3c60bc21993a3563321
Nick Hoffman <njhoffman1982@gmail.com>
2023-11-21 20:09:11 -0600

    fix: use winids for window reference

--- lua/nvim-navbuddy/lsp.lua
+++ lua/nvim-navbuddy/lsp.lua
@@ -1,18 +1,18 @@
-local navic = require("nvim-navic.lib")
-local nui_menu = require("nui.menu")
+local navic = require('nvim-navic.lib')
+local nui_menu = require('nui.menu')
 
-local menu_display = require("nvim-navbuddy.display.menu")
-local title_display = require("nvim-navbuddy.display.title")
-local state = require("nvim-navbuddy.state")
-local config = require("nvim-navbuddy.config")
+local menu_display = require('nvim-navbuddy.display.menu')
+local title_display = require('nvim-navbuddy.display.title')
+local state = require('nvim-navbuddy.state')
+local config = require('nvim-navbuddy.config')
 
 local function choose_lsp_menu(for_buf, make_request)
   local style = nil
 
-  if config.window.border ~= nil and config.window.border ~= "None" then
+  if config.window.border ~= nil and config.window.border ~= 'None' then
     style = config.window.border
   else
-    style = "single"
+    style = 'single'
   end
 
   local min_width = 23
@@ -20,7 +20,7 @@ local function choose_lsp_menu(for_buf, make_request)
 
   for _, v in ipairs(state.attached_clients[for_buf]) do
     min_width = math.max(min_width, #v.name)
-    table.insert(lines, nui_menu.item(v.id .. ":" .. v.name))
+    table.insert(lines, nui_menu.item(v.id .. ':' .. v.name))
   end
 
   local min_height = #lines
@@ -28,13 +28,13 @@ local function choose_lsp_menu(for_buf, make_request)
   local selection = nil
 
   local menu = nui_menu({
-    relative = "editor",
-    position = "50%",
+    relative = 'editor',
+    position = '50%',
     border = {
       style = style,
       text = {
-        top = "[Choose LSP Client]",
-        top_align = "center",
+        top = '[Choose LSP Client]',
+        top_align = 'center',
       },
     },
   }, {
@@ -42,15 +42,15 @@ local function choose_lsp_menu(for_buf, make_request)
     min_width = min_width,
     min_height = min_height,
     keymap = {
-      focus_next = { "j", "<Down>", "<Tab>" },
-      focus_prev = { "k", "<Up>", "<S-Tab>" },
-      close = { "<Esc>", "q", "<C-c>" },
-      submit = { "<CR>", "<Space>", "l" },
+      focus_next = { 'j', '<Down>', '<Tab>' },
+      focus_prev = { 'k', '<Up>', '<S-Tab>' },
+      close = { '<Esc>', 'q', '<C-c>' },
+      submit = { '<CR>', '<Space>', 'l' },
     },
     on_close = function() end,
     on_submit = function(item)
       selection =
-        { id = tonumber(string.match(item.text, "%d+")), name = string.sub(string.match(item.text, ":.+"), 2) }
+        { id = tonumber(string.match(item.text, '%d+')), name = string.sub(string.match(item.text, ':.+'), 2) }
       make_request(selection)
     end,
   })
@@ -103,12 +103,12 @@ local function menu_handler(bufnr, curr_node, lsp_name)
   if curr_node.is_root then
     if curr_node.children then
       local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-      local closest_dist = math.abs(curr_line - curr_node.children[1].scope["start"].line)
+      local closest_dist = math.abs(curr_line - curr_node.children[1].scope['start'].line)
       local closest_node = curr_node.children[1]
 
       for _, node in ipairs(curr_node.children) do
-        if math.abs(curr_line - node.scope["start"].line) < closest_dist then
-          closest_dist = math.abs(curr_line - node.scope["start"].line)
+        if math.abs(curr_line - node.scope['start'].line) < closest_dist then
+          closest_dist = math.abs(curr_line - node.scope['start'].line)
           closest_node = node
         end
       end
@@ -119,10 +119,11 @@ local function menu_handler(bufnr, curr_node, lsp_name)
     end
   end
 
+  local winid = vim.fn.bufwinid(bufnr)
   menu_display:new({
     for_buf = bufnr,
-    for_win = vim.api.nvim_get_current_win(),
-    start_cursor = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()),
+    for_win = winid,
+    start_cursor = vim.api.nvim_win_get_cursor(winid),
     focus_node = curr_node,
     config = config,
     lsp_name = lsp_name,
@@ -133,12 +134,12 @@ local function title_handler(bufnr, curr_node, lsp_name)
   if curr_node.is_root then
     if curr_node.children then
       local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-      local closest_dist = math.abs(curr_line - curr_node.children[1].scope["start"].line)
+      local closest_dist = math.abs(curr_line - curr_node.children[1].scope['start'].line)
       local closest_node = curr_node.children[1]
 
       for _, node in ipairs(curr_node.children) do
-        if math.abs(curr_line - node.scope["start"].line) < closest_dist then
-          closest_dist = math.abs(curr_line - node.scope["start"].line)
+        if math.abs(curr_line - node.scope['start'].line) < closest_dist then
+          closest_dist = math.abs(curr_line - node.scope['start'].line)
           closest_node = node
         end
       end
@@ -148,10 +149,11 @@ local function title_handler(bufnr, curr_node, lsp_name)
     end
   end
 
+  local winid = vim.fn.bufwinid(bufnr)
   title_display:new({
     for_buf = bufnr,
-    for_win = vim.api.nvim_get_current_win(),
-    start_cursor = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()),
+    for_win = winid,
+    start_cursor = vim.api.nvim_win_get_cursor(winid),
     focus_node = curr_node,
     config = config,
     lsp_name = lsp_name,
@@ -160,7 +162,7 @@ end
 
 local function open(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
-  if state.mode == "title" then
+  if state.mode == 'title' then
     request(bufnr, title_handler)
   else
     request(bufnr, menu_handler)
@@ -183,12 +185,12 @@ local function attach(client, bufnr)
   end
   table.insert(state.attached_clients[bufnr], client)
 
-  local navbuddy_augroup = vim.api.nvim_create_augroup("navbuddy", { clear = false })
+  local navbuddy_augroup = vim.api.nvim_create_augroup('navbuddy', { clear = false })
   vim.api.nvim_clear_autocmds({
     buffer = bufnr,
     group = navbuddy_augroup,
   })
-  vim.api.nvim_create_autocmd("BufDelete", {
+  vim.api.nvim_create_autocmd('BufDelete', {
     callback = function()
       navic.clear_buffer_data(bufnr)
       state.attached_clients[bufnr] = nil
@@ -197,18 +199,18 @@ local function attach(client, bufnr)
     buffer = bufnr,
   })
 
-  vim.api.nvim_buf_create_user_command(bufnr, "Navbuddy", function()
+  vim.api.nvim_buf_create_user_command(bufnr, 'Navbuddy', function()
     open(bufnr)
   end, {})
 end
 
 local function auto_attach(_config)
-  config = vim.tbl_deep_extend("force", config, _config)
-  local navbuddy_augroup = vim.api.nvim_create_augroup("navbuddy", { clear = false })
+  config = vim.tbl_deep_extend('force', config, _config)
+  local navbuddy_augroup = vim.api.nvim_create_augroup('navbuddy', { clear = false })
   vim.api.nvim_clear_autocmds({
     group = navbuddy_augroup,
   })
-  vim.api.nvim_create_autocmd("LspAttach", {
+  vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
       local bufnr = args.buf
       local client = vim.lsp.get_client_by_id(args.data.client_id)
